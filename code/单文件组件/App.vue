5552b1412d90f4280d4336d8b91ee0157a456dcf
Dragon01L <3325354115@qq.com>
2023-12-02 13:17:50 +0800

    提交说明

---
+++ code/单文件组件/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <student></student>
+  <School></School>
+</template>
+
+<script>
+import School from './School.vue'
+import Student from './Student.vue'
+export default {
+    name:'App',
+    components:{
+        Student,
+        School
+    }
+}
+</script>
+
+<style>
+
+</style>
